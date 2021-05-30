5629c3415148c338ac2faad6594adc15617e3b84
Matheus Rodrigues <matheusrg21@hotmail.com>
2021-05-30 12:22:29 -0300

    Rotas dinamicas e conflitos

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from fastapi import FastAPI
+import pydantic
 
 app = FastAPI()
 
+
 @app.get('/')
 def index():
-  return {'data': {'name': 'Matheus'}}
+  return {'data': 'Blog list'}
+
 
 @app.get('/about')
 def about():
   return {'data': 'about page'}
+
+
+# Rotas sem parametros devem vir primeiro, para nao quebrar na conversao
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+  return {'data': "all unpublished blogs"}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+  # fetch blog with id = id
+  return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+  # fetch comments of blog with id = id
+  return {'data': {'1', '2'}}
