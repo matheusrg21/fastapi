828ad80ff0a4da7174f8a2a183e3459338d5e218
Matheus Rodrigues <matheusrg21@hotmail.com>
2021-06-01 20:40:59 -0300

    Remove sonarlint warning

--- fastapi_app/hash.py
+++ fastapi_app/hash.py
@@ -1,9 +1,9 @@
 from passlib.context import CryptContext
-
+import pydantic
 
 pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class Hash():
-  def bcrypt(password: str):
+  def bcrypt(self, password: str):
     return pwd_cxt.hash(password)
