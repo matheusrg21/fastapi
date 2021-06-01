332e95f1aa0a0863b560460ba3c7852c405ee373
Matheus Rodrigues <matheusrg21@hotmail.com>
2021-06-01 16:40:05 -0300

    Restringindo conteúdo do response

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -2,6 +2,7 @@ from logging import FATAL
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, responses, status, Response
+from typing import List
 from . import schemas, models
 from .database import SessionLocal, engine
 
@@ -49,13 +50,13 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
   return {'Blog atualizado com sucesso'}
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
   blogs = db.query(models.Blog).all()
   return blogs
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
   blog = db.query(models.Blog).filter(models.Blog.id == id).first()
   if not blog:
