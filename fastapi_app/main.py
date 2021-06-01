5ba078239309c2c691ab59bc1f724e16280f0298
Matheus Rodrigues <matheusrg21@hotmail.com>
2021-06-01 16:02:46 -0300

    Métodos Delete e Update do blog

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,3 +1,4 @@
+from logging import FATAL
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, responses, status, Response
@@ -24,6 +25,28 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
   db.commit()
   db.refresh(new_blog)
   return new_blog
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+  blog = db.query(models.Blog).filter(models.Blog.id == id)
+  if not blog.first():
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f'Blog with id {id}')
+  blog.delete(synchronize_session=False)
+  db.commit()
+  return {'Blog removido com sucesso'}
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+  blog = db.query(models.Blog).filter(models.Blog.id == id).update(
+      {request.title, request.body})
+  if not blog.first():
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f'Blog with id {id}')
+  db.commit()
+  return {'Blog atualizado com sucesso'}
 
 
 @app.get('/blog')
