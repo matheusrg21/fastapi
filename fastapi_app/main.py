6e231fa030983f9a173f74cc7d5a6699c14f2126
Matheus Rodrigues <matheusrg21@hotmail.com>
2021-06-01 15:19:39 -0300

    Adicionando tratativa de resposta e status code

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,5 +1,6 @@
+from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, responses, status, Response
 from . import schemas, models
 from .database import SessionLocal, engine
 
@@ -16,7 +17,7 @@ def get_db():
     db.close()
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
   new_blog = models.Blog(title=request.title, body=request.body)
   db.add(new_blog)
@@ -31,7 +32,13 @@ def all(db: Session = Depends(get_db)):
   return blogs
 
 
-@app.get('/blog/{id}')
-def show(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
   blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+  if not blog:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f'Blog with the id {id} is not available.')
+    # response.status_code = status.HTTP_404_NOT_FOUND
+    # return {'detail': f'Blog with the id {id} is not available.'}
+
   return blog
