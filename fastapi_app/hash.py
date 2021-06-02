1dd1f0ad46f48de4411884a31f71bca0acf484b6
Matheus Rodrigues <matheusrg21@hotmail.com>
2021-06-01 21:31:06 -0300

    Adiciona autenticação

--- fastapi_app/hash.py
+++ fastapi_app/hash.py
@@ -7,3 +7,6 @@ pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash():
   def bcrypt(self, password: str):
     return pwd_cxt.hash(password)
+
+  def verify(self, hashed_password: str, clean_password: str):
+    return pwd_cxt.verify(clean_password, hashed_password)
---
+++ fastapi_app/routers/authentication.py
@@ -0,0 +1,23 @@
+from fastapi_app import database, models, schemas
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from fastapi_app.hash import Hash
+
+router = APIRouter(
+    tags=['Authentication']
+)
+
+
+@router.post('/login')
+def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+  user = db.query(models.User).filter(
+      models.User.email == request.username).first()
+  if not user:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f'Invalid credentials for {request.username}')
+
+  if Hash.verify(user.password, request.password):
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f'Invalid credentials for {request.username}')
+
+  return "login"
